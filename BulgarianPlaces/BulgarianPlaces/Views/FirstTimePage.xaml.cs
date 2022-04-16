9d100ebc6d24c3ca29dc43aa56e16e42d3549f5b
thunder913 <andon_00@abv.bg>
2022-04-16 14:06:51 +0300

    added FirstTimePage logic

--- BulgarianPlaces/BulgarianPlaces/Views/FirstTimePage.xaml.cs
+++ BulgarianPlaces/BulgarianPlaces/Views/FirstTimePage.xaml.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +19,10 @@ namespace BulgarianPlaces.Views
     {
         public string ImageBase64 { get; set; }
         private FirstTimeViewModel vm { get; set; }
+        private HttpClient client { get; set; }
         public FirstTimePage()
         {
+            client = new HttpClient();
             InitializeComponent();
             BindingContext = vm = new FirstTimeViewModel(image);
         }
@@ -32,6 +36,24 @@ namespace BulgarianPlaces.Views
                 await stream.ReadAsync(bytes, 0, (int)stream.Length);
                 ImageBase64 = Convert.ToBase64String(bytes);
                 vm.ChangeImage(ImageBase64);
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri(string.Format(GlobalConstants.Url + $"User/FinishFirstTime"));
+            var form = new MultipartFormDataContent();
+            form.Add(new StringContent(ImageBase64 ?? string.Empty), "image");
+            form.Add(new StringContent(vm.Description ?? string.Empty), "description");
+            form.Add(new StringContent(vm.FirstName ?? string.Empty), "firstName");
+            form.Add(new StringContent(vm.LastName ?? string.Empty), "lastName");
+            form.Add(new StringContent(Application.Current.Properties["token"].ToString()), "jwt");
+            form.Headers.ContentType.MediaType = "multipart/form-data";
+            var result = await client.PostAsync(uri, form);
+            var responseAsString = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                await Shell.Current.GoToAsync($"//{nameof(ProfilePage)}");
             }
         }
     }
